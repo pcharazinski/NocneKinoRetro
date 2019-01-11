38507250b866f3d720f234e158968841469d1198
pcharazinski <pawelcharazinski@gmail.com>
2019-01-11 04:50:15 +0100

    Axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
