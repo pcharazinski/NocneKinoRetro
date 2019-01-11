eb793c3390aba7fb79e1d0b212fcf9b709f46828
pcharazinski <pawelcharazinski@gmail.com>
2019-01-11 04:14:08 +0100

    Dodanie adresu requestu api i klucza

---
+++ src/Api.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const data_request = http://www.omdbapi.com/?apikey=29c10e44&;
+const poster = http://img.omdbapi.com/?apikey=29c10e44&;
+
+export default Api;
