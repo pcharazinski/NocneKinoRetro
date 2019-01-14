f3b79d7c249d90da617d2a96370beff4dd7c45c7
Unknown <marcinwos18@gmail.com>
2019-01-14 22:37:04 +0100

    omijamy cors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 
 mongoose.connect('mongodb://admin:coders1@ds253284.mlab.com:53284/nocnekino', { useNewUrlParser: true })
     .then(() => { console.log('jestem w bazie, badumtssss') })
     .catch(() => { console.log('popsulysmy') });
 const app = express();
+app.use(cors());// Wyłączyć przed deploymentem
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 const route = require('./routes/router')
