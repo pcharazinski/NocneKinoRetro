1cf8f761a215eb6ab5a4a17b1e9da818c3f44c39
pcharazinski <pawelcharazinski@gmail.com>
2019-01-15 14:57:28 +0100

    Dodanie klucza API i jquery

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Axios from 'axios';
 
 class App extends Component {
 
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import '../sass/App.scss'
 import Hero from './Hero';
+import $ from 'jquery';
 
 class App extends React.Component {
     constructor() {
-        super();
-        const pobierzDaneTest = () => {
-            fetch('/test').then((resp) => resp.json()).then(data => console.log(data)).catch(err => console.error(err));
-            // fetch('/test').then((resp) => console.log(resp))
+        super(props);
+        this.state = {}
+        this.search ("a") }
+
+            const pobierzDaneTest = (searchTerm) => {
+            // fetch('/test').then((resp) => resp.json()).then(data => console.log(data)).catch(err => console.error(err));
+            const urlDB = "https://api.themoviedb.org/3/search/movie?api_key=02b7c790eefcf035ffc889efdfc6cb77&query=" + searchTerm
+            
         }
         pobierzDaneTest();
     }
