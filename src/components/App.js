80961b461aa6e8e90b3a7a000259e558bf9c5d15
pcharazinski <pawelcharazinski@gmail.com>
2019-01-15 16:03:07 +0100

    Nie wiem, co dalej

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     constructor() {
         super(props);
         this.state = {}
-        this.pobierzDane ("a") }
+        this.pobierzDane ("retro") }
 
             const pobierzDane = (searchTerm) => {
             const urlDB = "https://api.themoviedb.org/3/search/movie?api_key=02b7c790eefcf035ffc889efdfc6cb77&query=" + searchTerm
@@ -19,26 +19,29 @@ class App extends React.Component {
                             
                   const movieRows = []
           
+                //   this.props.movie.id
+                //   this.props.movie.poster_src
+                //   this.props.movie.title
+                //   this.props.movie.overview
+
                   results.forEach((movie) => {
                     movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
-                    const movieRow = <MovieRow key={movie.id} movie={movie}/>
+                    const movieRow = <movieList key={movie.id} movie={movie}/>
                     movieRows.push(movieRow)
                   })
           
-                  this.setState({rows: movieRows})
-                },
-                error: (xhr, status, err) => {
-                  console.error("Bład pobierania danych")
+                  this.setState({rows: movieList})
                 }
+               
               })
             }
           
-            searchChangeHandler(event) {
-              console.log(event.target.value)
-              const boundObject = this
-              const searchTerm = event.target.value
-              boundObject.pobierzDane(searchTerm)
-            }
+            // searchChangeHandler(event) {
+            //   console.log(event.target.value)
+            //   const boundObject = this
+            //   const searchTerm = event.target.value
+            //   boundObject.pobierzDane(searchTerm)
+            // }
         
     render() {
 
