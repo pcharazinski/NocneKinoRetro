9481006c0075c9b48bc73cee9e479177cbce23ae
pcharazinski <pawelcharazinski@gmail.com>
2019-01-15 15:26:55 +0100

    Ciag dalszy ajaxa

--- src/components/App.js
+++ src/components/App.js
@@ -7,18 +7,44 @@ class App extends React.Component {
     constructor() {
         super(props);
         this.state = {}
-        this.search ("a") }
+        this.pobierzDane ("a") }
 
-            const pobierzDaneTest = (searchTerm) => {
+            const pobierzDane = (searchTerm) => {
             // fetch('/test').then((resp) => resp.json()).then(data => console.log(data)).catch(err => console.error(err));
             const urlDB = "https://api.themoviedb.org/3/search/movie?api_key=02b7c790eefcf035ffc889efdfc6cb77&query=" + searchTerm
-            
-        }
-        pobierzDaneTest();
-    }
+            $.ajax({
+                url: urlString,
+                success: (searchResults) => {
+                  console.log("Pobrano API")
+                  const results = searchResults.results
+                            
+                  const movieRows = []
+          
+                  results.forEach((movie) => {
+                    movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
+                    const movieRow = <MovieRow key={movie.id} movie={movie}/>
+                    movieRows.push(movieRow)
+                  })
+          
+                  this.setState({rows: movieRows})
+                },
+                error: (xhr, status, err) => {
+                  console.error("Bład pobierania danych")
+                }
+              })
+            }
+          
+            searchChangeHandler(event) {
+              console.log(event.target.value)
+              const boundObject = this
+              const searchTerm = event.target.value
+              boundObject.performSearch(searchTerm)
+            }
+        
     render() {
 
         return <Hero />;
+        // {this.state.XXX}
     }
 }
 
