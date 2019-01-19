0ef038de54e6276e01849db6218cca72d0fc111f
Unknown <marcinwos18@gmail.com>
2019-01-19 21:38:25 +0100

    Deploy

---
+++ build/precache-manifest.0fcb4e5861e6f525fa0312918222d338.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "639c477501a462a704953cf785248cc3",
+    "url": "static/media/arrow.639c4775.png"
+  },
+  {
+    "revision": "9471b31c2c36b43cacb4",
+    "url": "static/css/main.2b40a6c9.chunk.css"
+  },
+  {
+    "revision": "37f713d665ed3d332dfc",
+    "url": "static/js/1.37f713d6.chunk.js"
+  },
+  {
+    "revision": "91235dafa87037398966",
+    "url": "static/js/runtime~main.91235daf.js"
+  },
+  {
+    "revision": "27cde5753ae13562b2f106217913dddd",
+    "url": "static/media/rezerwuj.27cde575.png"
+  },
+  {
+    "revision": "9471b31c2c36b43cacb4",
+    "url": "static/js/main.9471b31c.chunk.js"
+  },
+  {
+    "revision": "663383616c48f43ff03d6ce547ed07a0",
+    "url": "static/media/potwierdz_btn.66338361.png"
+  },
+  {
+    "revision": "aebb818895e4c439200edccc8503fe4e",
+    "url": "static/media/logo.aebb8188.png"
+  },
+  {
+    "revision": "9fa387ffe6c968c69654132dad67c2e5",
+    "url": "static/media/bg-hero.9fa387ff.jpg"
+  },
+  {
+    "revision": "19183daa3b5c482be4bcd9ef170da823",
+    "url": "static/media/rezerwujBtn.19183daa.png"
+  },
+  {
+    "revision": "9e67a4572da4b996be949cb043ff7212",
+    "url": "index.html"
+  }
+];
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 const route = require('./routes/router')
 
-// app.use(express.static('public'));
+app.use(express.static('build'));
 app.use('/', route);
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Misiaczku, slucham Cie na porcie ${port}`));
